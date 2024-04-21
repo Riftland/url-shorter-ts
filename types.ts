5afc7e2e6cb53f2c27ba29c45af71e59bacd6bfd
Riftland <fran.mscr@gmail.com>
2024-04-21 21:15:08 +0200

    feat: created multiple types

--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { DatabasePool } from "slonik";
+import { DatabasePool, SlonikError } from "slonik";
 
 export type HttpError = {
   statusCode: number;
@@ -15,3 +15,30 @@ export type RoutesMiddleware = (db: Promise<DbPool>) => Router;
 export type ControllersMiddleware = (
   db?: Promise<DbPool>
 ) => (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+export type SuccessResponse<T> = {
+  error_code?: string;
+  content?: T;
+}
+
+export type FailureResponse = string;
+
+export type ModelResponse<T> = {
+  ok: boolean,
+  message?: string,
+  error_code?: string | undefined,
+  content?: T | undefined,
+};
+
+export type SuccessCB<T> = () => Promise<SuccessResponse<T> | void>;
+
+export type FailureCB<T> = (error: T) => FailureResponse;
+
+export type CatcherFn<T, U> = (successCB: SuccessCB<T>, failureCB: FailureCB<U>) => Promise<ModelResponse<T>>
+
+export type UserPayload = {
+  email: string;
+  username: string;
+}
+
+export type DBError = SlonikError;
