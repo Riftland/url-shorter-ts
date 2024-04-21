7b5b3f209362092424ae9204896b40299da765cd
Riftland <fran.mscr@gmail.com>
2024-04-21 21:18:24 +0200

    feat: added signup auth route

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { RoutesMiddleware } from "../types";
+import { fieldsValidator } from "../middlewares/validators";
 
 const router = Router();
 
+const signupValidator = fieldsValidator('email', 'username', 'password');
+
 const authRoutes: RoutesMiddleware = (db) => {
+  router.post('/signup', signupValidator, )
+
   return router;
 };
 
