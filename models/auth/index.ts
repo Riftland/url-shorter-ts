c341e2e1ec59dfe618ef1237747bf94ea71f2f79
Riftland <fran.mscr@gmail.com>
2024-04-24 01:15:51 +0200

    feat: added process functions of auth model

--- models/auth/index.ts
+++ models/auth/index.ts
@@ -1,15 +1,39 @@
-import { insertUser } from "./queries";
-import { DbPool } from "../../types";
-import { catcher, log } from "../../utils";
+import { insertUser, selectByEmail } from "./queries";
+import { DbError, DbPool, ErrorCodes, UserPayload } from "../../types";
+import { catcher, extract, nonOk, setContent, stdErrCB } from "../../utils";
+import { CREATE_USER_OPT, SELECT_USER_OPT } from "../../constants";
 
-export const createUser = (db: DbPool) => async (email: string, username: string, password: string) => {
-    return catcher(
-        async () => {
-            await db.query(insertUser(email, username, password));
-        },
-        (error) => {
-            log(`> create user error: ${error.message}`);
-            return error.message
-        }
-    )
-}
+export const createUser =
+    (db: DbPool) =>
+        async (email: string, username: string, password: string) =>
+            catcher<undefined, DbError>(
+                async () => {
+                    await db.query(insertUser(email, username, password));
+                },
+                stdErrCB(CREATE_USER_OPT)
+            );
+
+export const selectUser =
+    (db: DbPool) =>
+        async (
+            email: string,
+            compareFn: (hash: string) => Promise<boolean> // importar tipo desde la librería simple-stateless-auth-library-ts
+        ) =>
+            catcher<UserPayload | undefined, DbError>(
+                async () => {
+                    const user = await db.maybeOne(selectByEmail(email));
+
+                    if (!user) return nonOk(ErrorCodes.WRONG_DATA);
+
+                    const areEqual = await compareFn(user.password);
+
+                    if (!areEqual) return nonOk(ErrorCodes.WRONG_DATA);
+
+                    return setContent(
+                        extract<UserPayload, keyof UserPayload>(
+                            'email', 'username'
+                        )(user)
+                    );
+                },
+                stdErrCB(SELECT_USER_OPT)
+            )
