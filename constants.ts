d65f3f90f44f56150fe6782af8697bcf2290cb16
Riftland <fran.mscr@gmail.com>
2024-04-24 01:12:59 +0200

    chore: added constants to use in model error cb

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const CREATE_USER_OPT = 'create user error';
+export const SELECT_USER_OPT = 'select user error';
+export const REGISTER_URL_OPT = 'error registering a new url';
+export const RECOVER_ORIGIN_URL_OPT = 'error recovering origin url';
+export const RETRIEVE_SHORT_BY_EMAIL = 'error retrieving shorts by email';
