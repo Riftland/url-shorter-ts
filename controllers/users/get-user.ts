775f2473b8a5b82f72925660854efa1078cc671b
Riftland <fran.mscr@gmail.com>
2024-04-25 01:02:43 +0200

    feat: get-user controller completed

--- controllers/users/get-user.ts
+++ controllers/users/get-user.ts
@@ -1,7 +1,16 @@
+import { OK_STATUS } from "../../constants";
 import { ControllersMiddleware } from "../../types";
+import { setResponse } from "../../utils";
 
 export const getUser: ControllersMiddleware = () => async (_, res, __) => {
-  res.status(200).json({
-    success: true,
-  });
+  const { user } = res.locals;
+
+  // res.status(200).json({
+  //   success: true,
+  //   data: {
+  //     username: user.username,
+  //   }
+  // });
+
+  setResponse(res.status(OK_STATUS))({ username: user.username })
 };
