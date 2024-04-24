43193b32f33cecdf2eb1781a7bd793a12712ec1c
Riftland <fran.mscr@gmail.com>
2024-04-25 01:03:04 +0200

    feat: get-urls-creted by user controller completed

---
+++ controllers/users/get-urls-created.ts
@@ -0,0 +1,19 @@
+import httpErrors from "../../misc/errors";
+import { retrieveOriginUrl } from "../../models/auth/generator";
+import { ControllersMiddleware, DbPool, ErrorCodes } from "../../types";
+import { setRedirect } from "../../utils";
+
+export const getUrlsCreated: ControllersMiddleware = (db) => async (_, res, next) => {
+    const { user, search } = res.locals;
+
+    const response = await retrieveOriginUrl(
+        await db as DbPool
+    )(user.email, search);
+
+    if (!response.ok) return next(
+        httpErrors[response.error_code ??
+        ErrorCodes.SERVER_ERROR]
+    );
+
+    setRedirect(res)(response.content?.url as string);
+}
