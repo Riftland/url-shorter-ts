12fec679ac542b2769ef4b2ce23b0e4a591ea392
Riftland <fran.mscr@gmail.com>
2024-04-25 01:00:13 +0200

    chore: added code status as constants

--- constants.ts
+++ constants.ts
@@ -3,3 +3,5 @@ export const SELECT_USER_OPT = 'select user error';
 export const REGISTER_URL_OPT = 'error registering a new url';
 export const RECOVER_ORIGIN_URL_OPT = 'error recovering origin url';
 export const RETRIEVE_SHORT_BY_EMAIL = 'error retrieving shorts by email';
+export const OK_STATUS = 200;
+export const REDIRECTION_STATUS = 307;
