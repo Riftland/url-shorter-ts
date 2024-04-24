cde929b10f90ecf3c5c32912768d97e0ddf74f9b
Riftland <fran.mscr@gmail.com>
2024-04-25 01:05:00 +0200

    feat: added all implemented routes in auth routes selector

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { RoutesMiddleware } from "../types";
-import { fieldsValidator } from "../middlewares/validators";
+import { authorizer, fieldsValidator } from "../middlewares";
+import { signin, signup } from "../controllers/auth";
 
 const router = Router();
 
 const signupValidator = fieldsValidator('email', 'username', 'password');
+const signinValidator = fieldsValidator('email', 'username');
 
 const authRoutes: RoutesMiddleware = (db) => {
-  router.post('/signup', signupValidator, )
+  router.post('/signup', signupValidator, signup(db));
+  router.post('/signin', signinValidator, signin(db));
+  router.post('/signout', authorizer(), signin(db));
 
   return router;
 };
