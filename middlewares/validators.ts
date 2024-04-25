0b8c5c1cc2b77f16f33a6050e37692e3fca04b8b
Riftland <fran.mscr@gmail.com>
2024-04-26 00:19:04 +0200

    fix: changed error types in validator middleware

--- middlewares/validators.ts
+++ middlewares/validators.ts
@@ -4,10 +4,12 @@ import { ErrorCodes } from '../types';
 
 export const fieldsValidator = (...fields: string[]) => (req: Request, _: Response, next: NextFunction) => {
     for (let field of fields) {
-        if (!req.body[field]) return next(httpErrors[ErrorCodes.NOT_FOUND]);
+        if (!req.body[field]) return next(httpErrors[ErrorCodes.BAD_REQUEST]);
     }
 
     const { password } = req.body;
 
-    return password && password.length < 4 ? next(httpErrors["pass_length"]) : next();
+    return password && password.length < 4 ?
+        next(httpErrors[ErrorCodes.PASS_LENGTH]) :
+        next();
 }
