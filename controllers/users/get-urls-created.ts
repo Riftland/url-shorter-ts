4a4ee540ab81e33905e3b0e6e72243d5ab0a9397
Riftland <fran.mscr@gmail.com>
2024-04-26 00:17:37 +0200

    fix: changed what did old getUrlsCreated controller

--- controllers/users/get-urls-created.ts
+++ controllers/users/get-urls-created.ts
@@ -1,19 +1,27 @@
+import { API_URL, OK_STATUS } from "../../constants";
 import httpErrors from "../../misc/errors";
-import { retrieveOriginUrl } from "../../models/auth/generator";
-import { ControllersMiddleware, DbPool, ErrorCodes } from "../../types";
-import { setRedirect } from "../../utils";
+import { retrieveShortsByUser } from "../../models/auth/generator";
+import { ControllersMiddleware, DbPool, ErrorCodes, Payload, ShortsByEmail } from "../../types";
+import { setResponse } from "../../utils";
 
 export const getUrlsCreated: ControllersMiddleware = (db) => async (_, res, next) => {
-    const { user, search } = res.locals;
+    const { user } = res.locals;
 
-    const response = await retrieveOriginUrl(
+    const response = await retrieveShortsByUser(
         await db as DbPool
-    )(user.email, search);
+    )(user.email);
 
-    if (!response.ok) return next(
-        httpErrors[response.error_code ??
-        ErrorCodes.SERVER_ERROR]
-    );
+    if (!response.ok) return next(httpErrors[ErrorCodes.SERVER_ERROR]);
 
-    setRedirect(res)(response.content?.url as string);
+    const payload: Payload = {
+        shorts: (response.content as ShortsByEmail).map(({ short_url, uses, uses_by_creator }) => ({
+            url: `${API_URL}${short_url}`,
+            yours: uses_by_creator,
+            overall: uses,
+        }))
+    }
+
+    setResponse<Payload>(
+        res.status(OK_STATUS)
+    )(payload)
 }
