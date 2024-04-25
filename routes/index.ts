6ac7f009c762e49be8c983032f4fc260d185ce83
Riftland <fran.mscr@gmail.com>
2024-04-26 00:22:05 +0200

    feat: added former routes

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { RoutesMiddleware } from "../types";
 import usersRoutes from "./users";
+import authRoutes from './auth';
+import generatorRoutes from "./generator";
 
 const router = Router();
 
 const routesDispatcher: RoutesMiddleware = (db) => {
-  router.use("/users", usersRoutes(db));
+  router.use('/auth', authRoutes(db));
+  router.use('/users', usersRoutes(db));
+  router.use('/short', generatorRoutes(db));
 
   return router;
 };
