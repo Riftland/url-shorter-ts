58fd4661209634c6a7559dbd8726896dde2d157f
Riftland <fran.mscr@gmail.com>
2024-04-26 00:16:41 +0200

    feat: created controller to generate short url

---
+++ controllers/generator/create-short-url.ts
@@ -0,0 +1,21 @@
+import { API_URL, OK_STATUS } from "../../constants";
+import httpErrors from "../../misc/errors";
+import { registerNewUrl } from "../../models/auth/generator";
+import { ControllersMiddleware, DbPool, ErrorCodes } from "../../types";
+import { genToken, setResponse } from "../../utils";
+
+export const createShortUrl: ControllersMiddleware = (db) => async (req, res, next) => {
+    const { url } = req.body;
+    const { user } = res.locals;
+    const randomPath = `/${genToken()}`;
+
+    const response = await registerNewUrl(
+        await db as DbPool
+    )(user.email, randomPath, url);
+
+    if (!response.ok) return next(httpErrors[ErrorCodes.SERVER_ERROR]);
+
+    setResponse(res.status(OK_STATUS))(
+        { url: `${API_URL}${randomPath}` }
+    )
+}
