02268f9b3f70e9051741accd405d3aed7c5d12b0
Riftland <fran.mscr@gmail.com>
2024-04-26 00:21:06 +0200

    fix: managed types in auth routes

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import { RoutesMiddleware } from "../types";
 import { authorizer, fieldsValidator } from "../middlewares";
-import { signin, signup } from "../controllers/auth";
+import { signin, signout, signup } from "../controllers/auth";
 
 const router = Router();
 
 const signupValidator = fieldsValidator('email', 'username', 'password');
-const signinValidator = fieldsValidator('email', 'username');
+const signinValidator = fieldsValidator('email', 'password');
 
 const authRoutes: RoutesMiddleware = (db) => {
   router.post('/signup', signupValidator, signup(db));
   router.post('/signin', signinValidator, signin(db));
-  router.post('/signout', authorizer(), signin(db));
+  router.post('/signout', authorizer(), signout(db));
 
   return router;
 };
